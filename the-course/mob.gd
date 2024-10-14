6ed416498cb1656def4be509df3e9d7d15eb5277
erin.roglaska@gmail.com <erin.rogalska@gmail.com>
2024-10-14 16:51:26 +0100

    not working enemy with animations

---
+++ the-course/mob.gd
@@ -0,0 +1,19 @@
+extends RigidBody2D
+
+var velocity = 200
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta:) :
+	pass
+	
+	if transform.x = 0:
+		$AnimatedSprite2D.animation = "standing"
+		$AnimatedSprite2D.flip_v = false
+		# See the note below about the following boolean assignment.
+		$AnimatedSprite2D.flip_h = velocity.x < 0
