7c5747b0182936051bf7ba512062b9d38fcb0646
erin.roglaska@gmail.com <erin.rogalska@gmail.com>
2024-10-21 14:23:51 +0100

    printing positons and values of the player

--- the-course/player.gd
+++ the-course/player.gd
@@ -8,6 +8,20 @@ const JUMP_VELOCITY = -100.0
 
 const TURN_RATE = 180
 
+func _print_stuff():
+	DebugDraw2D.set_text("pos", position)
+	DebugDraw2D.set_text("rotation", rotation)
+	DebugDraw2D.set_text("rotation", rad_to_deg(rotation))
+	DebugDraw2D.set_text("transform.x", transform.x)
+	DebugDraw2D.set_text("transform.y", transform.y)
+	DebugDraw2D.set_text("global_transform.x", global_transform.x)
+	DebugDraw2D.set_text("global_transform.y", global_transform.y)
+	DebugDraw2D.set_text("vel", velocity)
+	
+	
+func _process(delta: float) -> void:
+	_print_stuff()
+
 func _physics_process(delta: float) -> void:
 	
 	var r = Input.get_axis("turn_left", "turn_right")
